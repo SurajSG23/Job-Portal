9801dfa9373b1c5b736ff55a016c263daf6256a1
Suraj S G Dhanva <surajsgd23@gmail.com>
2024-12-13 11:19:38 +0530

    api key updated

--- components/firebaseauth.js
+++ components/firebaseauth.js
@@ -3,8 +3,13 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    
-};
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
