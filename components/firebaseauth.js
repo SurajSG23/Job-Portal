78a3b1b577521f2b2cf6b408652738c998da8ba5
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-07-15 04:14:22 +0530

    Refactor Firebase configuration and enhance UI with new sign-up button

--- components/firebaseauth.js
+++ components/firebaseauth.js
@@ -11,13 +11,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCO9lSfFC7DJJMYQMAdbPXx-Iapw4Qzen0",
+  authDomain: "jobportal-3f013.firebaseapp.com",
+  projectId: "jobportal-3f013",
+  storageBucket: "jobportal-3f013.firebasestorage.app",
+  messagingSenderId: "295505547079",
+  appId: "1:295505547079:web:c2b981cacdbdde80748662",
 };
+console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
