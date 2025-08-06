2fb5853f6904017f8144a50b9a732cd9bc69d7fc
Diya H.Prajapati <diyap2359@gmail.com>
2025-08-06 13:27:36 +0530

    js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function Redirect(){
     window.location.href = "components/homepage.html"
 }
+function toggleBookmark(button) {
+  button.classList.toggle('saved');
+  const icon = button.querySelector('i');
+  if (button.classList.contains('saved')) {
+    icon.classList.remove('fa-regular');
+    icon.classList.add('fa-solid');
+  } else {
+    icon.classList.remove('fa-solid');
+    icon.classList.add('fa-regular');
+  }
+}
