f77e32b897db118d113b8e36bebf2e652b8c1ec3
Suraj S G Dhanva <surajsgd23@gmail.com>
2025-08-07 20:16:31 +0530

    UI fix

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-let sign_in_btn=document.getElementsByClassName("sign-in")[0];
-let sign_up_btn=document.getElementsByClassName("sign-up")[0];
-let sign_up_section=document.getElementsByClassName("sign-section")[0]
-let sign_in_section=document.getElementsByClassName("sign-section-2")[0]
+let sign_in_btn = document.getElementsByClassName("sign-in")[0];
+let sign_up_btn = document.getElementsByClassName("sign-up")[0];
+let sign_up_section = document.getElementsByClassName("sign-section")[0];
+let sign_in_section = document.getElementsByClassName("sign-section-2")[0];
 
+sign_in_btn.addEventListener("click", () => {
+    sign_up_section.style.visibility = "hidden";
+    sign_in_section.style.visibility = "visible";
+});
 
-sign_in_btn.addEventListener("click",()=>{
-    sign_up_section.style.visibility="hidden";
-    sign_in_section.style.visibility="visible";
-})
+sign_up_btn.addEventListener("click", () => {
+    sign_in_section.style.visibility = "hidden";
+    sign_up_section.style.visibility = "visible";
+});
 
-sign_up_btn.addEventListener("click",()=>{
-    sign_in_section.style.visibility="hidden";
-    sign_up_section.style.visibility="visible";
-})
+function guestLogin() {
+    window.location.href = "components/homepage.html";
+}
